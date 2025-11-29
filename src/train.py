560a6ed24b53b84cc8ab1af16a2f76679a37646b
Blackbi <ongbivlog3107@gmail.com>
2025-11-29 16:58:03 +0700

    add config

--- src/train.py
+++ src/train.py
@@ -4,5 +4,9 @@ import yaml
 from src.data import DataPrep
 from src.layers import Embedding, Flatten, Linear, Tanh, BatchNorm1d
 
-
+def main():
+    # Load the config
+    with open('config.yaml', 'r') as f:
+        config = yaml.safe_load(f)
+    
 
