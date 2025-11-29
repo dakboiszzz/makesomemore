9792e0d82734097674f8c7199c09d402fa10c7fd
Blackbi <ongbivlog3107@gmail.com>
2025-11-29 17:19:01 +0700

    Train the model

--- src/train.py
+++ src/train.py
@@ -3,6 +3,7 @@ import sys
 import yaml
 from src.data import DataPrep
 from src.layers import Embedding, Flatten, Linear, Tanh, BatchNorm1d
+import torch.nn.functional as F
 
 def main():
     # Load the config
@@ -49,6 +50,40 @@ def main():
     parameters = []
     for layer in layers:
         parameters += layer.parameters()
+
+    # Now we train
+    def train_model(max_steps,batch_size):
+        for i in range(max_steps):
+            # Construting the mini-batch
+            ix = torch.randint(0,X.shape[0],(batch_size,), generator = g)
+            X_batch, Y_batch = X[ix], Y[ix]
+
+            # Set the gradient first
+            for p in parameters:
+                p.requires_grad = True
+
+            # Implement the forward pass
+            activations = X_batch
+            for layer in layers:
+                activations = layer(activations)
+            loss = F.cross_entropy(activations,Y_batch)
+
+            # Implement the backward pass
+            for p in parameters:
+                p.grad = None
+            loss.backward()
+
+            # Update
+            lr = 0.01 if i < 20000 else 0.001
+            for p in parameters:
+                p.data += -lr * p.grad
+            if i % 10000 == 0:
+                print(f'Step {i}, Loss: {loss.item():.4f}')
+
+        print(f'Final loss: {loss}')
+
+
+    train_model(max_steps=100000, batch_size=32)
 
 
     
