0fd40374ab1076518ad26017808e611e04e8026f
Blackbi <ongbivlog3107@gmail.com>
2025-12-04 09:18:54 +0700

    Create load_checkpoint

--- src/utils.py
+++ src/utils.py
@@ -18,4 +18,21 @@ def save_checkpoint(model,data_prep,filepath):
     torch.save(checkpoint,filepath)
     print(f'Checkpoint saved to {filepath}')
 
-
+def load_checkpoint(filepath):
+    checkpoint = torch.load(filepath)
+    from src.model import Makemore
+    # Reconstruct the model
+    model = Makemore(
+        vocab_size = checkpoint['vocab_size'],
+        block_size = checkpoint['block_size'],
+        emb_size = checkpoint['emb_size'],
+        n_hidden = checkpoint['n_hidden'],
+        n_blocks = checkpoint['n_blocks']
+    )
+    # Load parameters
+    for i, layer in enumerate(model.layers):
+        for j, p in enumerate(layer.parameters()):
+            key = f'layer_{i}_param_{j}'
+            if key in checkpoint['model_state']:
+                p.data = checkpoint['model_state'][key]
+    return model, checkpoint['itos'], checkpoint['stoi'], checkpoint['block_size']
