38376688e0ed30ef4d213eb42b6da0c6c3b22d21
Blackbidz <ongbivlog3107@gmaill.com>
2025-12-04 09:51:54 +0000

    Change the setup

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
-
 from setuptools import setup, find_packages
 
 setup(
-    name="name_generator",
-    packages=find_packages(),
+    name="makesomemore",
+    version="0.1.0",
+    packages=find_packages(), # This automatically finds 'src' and sub-packages
+    install_requires=[
+        "torch",
+        "numpy",
+        "fastapi",
+        "uvicorn",
+        "pyyaml"
+    ],
 )
